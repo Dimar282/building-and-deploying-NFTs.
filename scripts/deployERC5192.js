1cee4285659ab84eaf4cc8afb99ab6f2e4eb2882
kaill13492 <50420867+kaill13492@users.noreply.github.com>
2026-01-17 16:39:13 +0100

    Fix contract name and log message in deployment script

--- scripts/deployERC5192.js
+++ scripts/deployERC5192.js
@@ -1,17 +1,18 @@
-```javascript
 const hre = require("hardhat");
 
 async function main() {
-  const ERC512 = await hre.ethers.getContractFactory("SoulboundERC5192");
+  const ERC5192 = await hre.ethers.getContractFactory("SoulboundERC5192");
   const sbt = await ERC5192.deploy();
 
   await sbt.waitForDeployment();
 
-  console.log(ERC-5192 Soulbound NFT deployed to:", await sbt.getAddress());
+  console.log(
+    "ERC-5192 Soulbound NFT deployed to:",
+    await sbt.getAddress()
+  );
 }
 
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-```
