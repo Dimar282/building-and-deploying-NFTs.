6b026cafb372d8afaa0a7b90c14b92c1992ffd53
Dimar282 <88005951+Dimar282@users.noreply.github.com>
2026-01-17 16:25:37 +0100

    Create NFT1155.sol

---
+++ contracts/NFT1155.sol
@@ -0,0 +1,32 @@
+```solidity
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT1155 is ERC1155, Ownable {
+    uint256 public constant GOLD = 1;
+    uint256 public constant SILVER = 2;
+
+    constructor() ERC1155("https://your-domain.com/metadata/{id}.json") {}
+
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(to, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
+```
